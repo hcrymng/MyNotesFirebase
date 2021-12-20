5e1a31b8bc0cfe79361254e97c079a6e0b396b0e
hcrymng <haceryiming@gamil.com>
2021-12-20 11:41:28 +0300

    .

---
+++ app/src/main/java/hcr/ymng/mynotesfirebase/Note.java
@@ -0,0 +1,33 @@
+package hcr.ymng.mynotesfirebase;
+
+import com.google.firebase.Timestamp;
+
+public class Note {
+    private String id;
+    private Timestamp date;
+    private String content;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
